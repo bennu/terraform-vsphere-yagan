32a23fd73cc289243b5cdb9b5ae2581551e97e09
Pierre Ugaz <ulm0@innersea.xyz>
2020-11-17 16:14:32 -0300

    chore: versions file

---
+++ versions.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    helm = {
+      source = "hashicorp/helm"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+    template = {
+      source = "hashicorp/template"
+    }
+    tls = {
+      source = "hashicorp/tls"
+    }
+    vsphere = {
+      source = "hashicorp/vsphere"
+    }
+  }
+  required_version = ">= 0.13"
+}
