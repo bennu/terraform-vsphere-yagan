c4ece96b79f839e4e9bb4e3db220e1cf7efd1d61
Pierre Ugaz <ulm0@innersea.xyz>
2020-11-17 16:12:45 -0300

    chore: kubernetes + addons

--- local.tf
+++ local.tf
@@ -42,4 +42,17 @@ locals {
       ]
     )
   }
+
+  enable_addons  = split(",", var.addons)
+  enable_dex     = contains(local.enable_addons, "dex")
+  enable_gangway = contains(local.enable_addons, "gangway")
+  dex_url        = format("dex.%s", var.dns_zone)
+  gangway_url    = format("gangway.%s", var.dns_zone)
+  oidc_extra_args = {
+    oidc-client-id      = "gangway"
+    oidc-groups-claim   = "groups"
+    oidc-issuer-url     = format("https://%s", local.dex_url)
+    oidc-username-claim = "name"
+  }
+  kube_api_extra_args = local.enable_gangway ? merge(local.oidc_extra_args, var.kube_api_extra_args) : var.kube_api_extra_args
 }
