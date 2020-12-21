d1b4185769d4fb3332eb8a1fc5c4620f24b36a02
Julián Véliz <julianvelizc@outlook.com>
2020-12-21 13:06:15 -0500

    chore: build only vms with terraform vsphere provider

--- output.tf
+++ output.tf
@@ -1,9 +1,2 @@
 output private_key { value = module.vsphere.private_key }
 output nodes { value = module.vsphere.nodes }
-# output controlplane { value = var.controlplane }
-
-output api_server_url { value = module.kubernetes.api_server_url }
-output client_cert { value = module.kubernetes.client_cert }
-output client_key { value = module.kubernetes.client_key }
-output cluster_ca_certificate { value = module.kubernetes.ca_crt }
-output username { value = module.kubernetes.kube_admin_user }
--- versions.tf
+++ versions.tf
@@ -1,23 +1,5 @@
 terraform {
   required_providers {
-    helm = {
-      source = "hashicorp/helm"
-    }
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-    }
-    null = {
-      source = "hashicorp/null"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-    template = {
-      source = "hashicorp/template"
-    }
-    tls = {
-      source = "hashicorp/tls"
-    }
     vsphere = {
       source = "hashicorp/vsphere"
     }
