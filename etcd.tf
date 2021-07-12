d25dbf7dee5cf6d3867776d9508c2ca7a126a36e
cdely.bennu <cesar.dely@bennu.cl>
2021-07-12 12:38:22 -0400

    testing disk issue

--- etcd.tf
+++ etcd.tf
@@ -17,7 +17,7 @@ resource "vsphere_virtual_machine" "etcd" {
     eagerly_scrub    = data.vsphere_virtual_machine.template.disks.0.eagerly_scrub
     label            = "disk0"
     size             = lookup(var.etcd, "disk_size")
-    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+    #thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
   }
 
   network_interface {
